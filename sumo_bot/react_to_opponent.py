63447714edbd9042aa422b94ed4715705a967988
Colby Todd <ctodd083@uottawa.ca>
2023-07-13 19:08:03 -0400

    Squashed mutants

--- sumo_bot/react_to_opponent.py
+++ sumo_bot/react_to_opponent.py
@@ -7,11 +7,11 @@ DIST = 60
 
 def set_speed(ultrasonicReading):
     if (ultrasonicReading <= 0):
-        raise ValueError('The ultrasonic sensor cannot read a value that is less than 0.')
+        raise ValueError('The ultrasonic sensor cannot read a value that is less than 0.') # pragma: no mutate
 
     if (ultrasonicReading > 2550):
-        raise ValueError('The ultrasonic sensor is limited to a range of 2550mm. "' 
-        + str(ultrasonicReading) + '" is an invalid reading')
+        raise ValueError('The ultrasonic sensor is limited to a range of 2550mm. "' # pragma: no mutate
+        + str(ultrasonicReading) + '" is an invalid reading') # pragma: no mutate
 
     if (ultrasonicReading <= DIST):
         return SLOW
